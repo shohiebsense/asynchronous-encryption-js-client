cde0862a38ba437aa49a42d4021a41d770131083
Shohieb <shohiebsense@users.noreply.github.com>
2024-12-01 15:59:54 +0700

    feat/add-crt-client-for-tls-check

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const forge = require("node-forge");
+const https = require('https')
+const fs = require('fs')
+const path = require("path");
 
+// Define your agent for HTTPS with custom options
+// const agent = new https.Agent({
+//   rejectUnauthorized: false,
+//   crt: fs.readFileSync("client_mismatch_cert.pem"),
+//   key: fs.readFileSync("client_mismatch_private.key"),
+// });
+
+
+
+const agent = new https.Agent({
+  cert: fs.readFileSync("client.crt"),
+  key: fs.readFileSync("client.key"),
+  ca: fs.readFileSync("ca.crt"),
+  rejectUnauthorized: true, // Ensure client validates server's certificate
+});
 async function fetchPublicKey() {
   const fetch = await import("node-fetch");
-  const response = await fetch.default("http://localhost:8080/public-key");
+ const response = await fetch.default("https://localhost:8080/public-key", {
+   method: "GET",
+   headers: {
+     Authorization: "Bearer your-jwt-token",
+   },
+   agent,
+    });
   if (!response.ok) {
     throw new Error("Failed to fetch public key");
   }
@@ -21,18 +45,19 @@ async function main() {
     try {
         const publicKeyPEM = await fetchPublicKey();
 
-        const amount = "5000";
+        const amount = "1";
 
         const encryptedAmountBase64 = encryptAmount(publicKeyPEM, amount);
-
-        const response = await fetch('http://localhost:8080/purchase', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                amount: encryptedAmountBase64,
-            }),
+        const fetch = await import("node-fetch");
+        const response = await fetch.default("https://localhost:8080/purchase", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            amount: encryptedAmountBase64,
+          }),
+          agent,
         });
 
         if (!response.ok) {
